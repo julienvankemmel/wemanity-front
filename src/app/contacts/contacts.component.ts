1e79e2e6c322b03bc0af76ead42aaf7f80e3da7d
Julien Vankemmel <julienvankemmel@MacBook-Pro-de-Julien.local>
2020-11-27 11:01:35 +0100

    Add paginated table, filter and edit button

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { PhonebookService } from '../services/phonebook.service';
+import {MatTableDataSource} from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-contacts',
@@ -8,14 +10,30 @@ import { PhonebookService } from '../services/phonebook.service';
 })
 export class ContactsComponent implements OnInit {
 
+  displayedColumns: string[] = ['firstname', 'lastname', 'phone', 'action'];
+  
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+
   constructor(private  phonebookService:PhonebookService) { }
+  
+  dataSource = new MatTableDataSource<Element>();
   contactList:any;
+  
   ngOnInit(): void {
     this.contactList = this.phonebookService.getPhonebookList().subscribe(
       data => {
         this.contactList = data;
+        this.dataSource.data = this.contactList;
       }
     )
   }
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  applyFilter(filterValue: string){
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
